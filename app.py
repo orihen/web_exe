96d0666edb4c37e35f9448a6854accc043f7d94a
orihen <orihen12@gmail.com>
2021-12-09 19:58:10 +0200

    upload exe7

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-
+@app.route('/home')
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_func():
     return 'Hello World!'
+
+@app.route('/catalog')
+def catalog_func():
+    return 'Welcome to catalog Page'
+
+@app.route('/about')
+def about_func():
+    return redirect('/catalog')
+
+@app.route('/contact')
+def contact_func():
+    return redirect(url_for('hello_func'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
