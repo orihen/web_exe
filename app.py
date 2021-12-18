f08db4df880aa6194aae611675ff8e338e281800
orihen <orihen12@gmail.com>
2021-12-18 21:10:44 +0200

    upload exe8

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for , render_template
 
 app = Flask(__name__)
-
-@app.route('/home')
-@app.route('/')
-def hello_func():
-    return 'Hello World!'
-
-@app.route('/catalog')
-def catalog_func():
-    return 'Welcome to catalog Page'
-
-@app.route('/about')
-def about_func():
-    return redirect('/catalog')
-
-@app.route('/contact')
-def contact_func():
-    return redirect(url_for('hello_func'))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/main')
+@app.route('/')
+def main_func():
+    return render_template('cv-main.html')
+
+@app.route('/GetinTouch')
+def about_func():
+    return render_template('GetinTouch.html')
+
+
+@app.route('/assignment8')
+def assignment8_func():
+    return render_template('assignment8.html',
+                           profile= {'name': 'Ori','second_name':'Hen'} ,
+                           Favorite_song= 'Macarena',
+                           hobbies = ('art', 'music', 'animals', 'web')
+                           )
